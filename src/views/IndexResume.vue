6fd7ce5363ab02b106147dbcd83548dd1908de37
Sam Sharff <samanthaleec@gmail.com>
2022-05-13 14:26:38 -0400

    adds checkbox to Trainings html, and creates function selecting checked data

--- src/views/IndexResume.vue
+++ src/views/IndexResume.vue
@@ -6,6 +6,7 @@ export default {
     return {
       experiences: [],
       trainings: [],
+      checked: false,
     };
   },
   created: function () {
@@ -22,7 +23,12 @@ export default {
     consoleLog: function () {
       this.experiences.forEach((experience) => {
         if (experience.checked) {
-          console.log("this is checked, hooray!", experience);
+          console.log("this experience is checked, hooray!", experience);
+        }
+      });
+      this.trainings.forEach((training) => {
+        if (training.checked) {
+          console.log("this training is checked, hooray!", training);
         }
       });
     },
@@ -51,6 +57,7 @@ export default {
       <br />
 
       <!-- INDEX EXPERIENCES -->
+      <!-- CHECKBOXES EXPERIENCES -->
 
       <div v-for="experience in experiences" v-bind:key="experience.id">
         <h1>{{ experience.category }}</h1>
@@ -58,33 +65,36 @@ export default {
         <h4>Description: {{ experience.description }}</h4>
         <h4>Tags: {{ experience.misc }}</h4>
         <a v-bind:href="`/resumes/${experience.id}`">More info</a>
-
-        <!-- CHECKBOXES EXPERIENCES -->
-
         <div class="form-check">
           <input
             class="text-justify form-check-input"
             v-model="experience.checked"
             type="checkbox"
-            value=""
-            id="flexCheckDefault"
+            value="Experience"
+            id="flexCheckDefault1"
           />
           <label class="form-check-label" for="flexCheckDefault">Export</label>
-          <!-- CHECKBOXES TRAININGS -->
         </div>
-        <br />
-        <br />
-        <br />
       </div>
+
+      <!-- INDEX TRAININGS -->
+      <!-- CHECKBOXES TRAININGS -->
+
       <div v-for="training in trainings" v-bind:key="training.id">
         <h1>{{ training.category }}</h1>
         <h4>Degree or Certification: {{ training.degree_or_cert }}</h4>
         <h4>Tags: {{ training.misc }}</h4>
         <a v-bind:href="`/trainings/${training.id}`">More info</a>
-        <br />
-        <br />
-        <br />
+        <input
+          class="text-justify form-check-input"
+          v-model="training.checked"
+          type="checkbox"
+          value="Training"
+          id="flexCheckDefault"
+        />
+        <label class="form-check-label" for="flexCheckDefault">Export</label>
       </div>
+      <!-- CONSOLELOG BUTTON -->
       <button v-on:click="consoleLog()">Console Log</button>
     </div>
   </div>
